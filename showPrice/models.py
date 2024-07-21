cf255f99a24eb3f33d7b2f9e714e845870abc7f4
pooriya <adibradp@gmail.com>
2024-07-21 20:30:44 +0330

    i work with ajax

--- showPrice/models.py
+++ showPrice/models.py
@@ -5,9 +5,10 @@ from django_jalali.db import models as jalali_models
 
 class Currency(models.Model):
     name = models.CharField(max_length=50)
-    sell=models.DecimalField(max_digits=10,decimal_places=3)
-    buy=models.DecimalField(max_digits=10,decimal_places=3)
+    sell=models.DecimalField(max_digits=10,decimal_places=1)
+    buy=models.DecimalField(max_digits=10,decimal_places=1)
     pubDate=jalali_models.jDateField(auto_now_add=True)
+    category=models.ForeignKey('category', on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.name
 class category(models.Model):
--- showPrice/views.py
+++ showPrice/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import pandas as pd
 from . import models
 import jdatetime
 # Create your views here.
 def index(request):
     today=jdatetime.date.today()
-    time=jdatetime.datetime.now().hour,jdatetime.datetime.now().minute,jdatetime.datetime.now().second
-    time=f'{time[0]}:{time[1]}:{time[2]}'
-    allCurrency = models.Currency.objects.filter(pubDate=today).all()
-    allcategory=models.category.objects.all()
-    return render(request=request,template_name='index.html',context={'currencies':allCurrency,'categories':allcategory,'today':today,'time':time})
+    allcategory = models.category.objects.all()
+    allCurrency = models.Currency.objects.filter(category=allcategory[0]).filter(pubDate=today).all()
+
+    return render(request=request,template_name='index.html',context={'currencies':allCurrency,'categories':allcategory,'today':today})
+def requestCurrency(request):
+    if request.method == 'POST':
+        clock=request.POST['clock']
+        category=request.POST['category']
+        if clock !='':
+            Currency = models.Currency.objects.filertDat(pubDate=clock).all()
+            category1 = models.category.objects.filter(category=category).all()
+            return JsonResponse({'data':Currency,'category':category1})
+        else:
+            return JsonResponse({'data':False})
+    else:
+        return JsonResponse({'data':False})
